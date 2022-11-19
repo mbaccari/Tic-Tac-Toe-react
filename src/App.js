0e4e167247ab6335ddab0fab86334d64daf77a06
Marshall Baccari <mbaccari@uw.edu>
2022-11-19 10:20:22 -0800

    visually updated board and centered in screen

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import Board from './components/Board';
 import Score from './components/Score';
 
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap'
+import styles from './App.module.css'
+
 function App() {
 
   const [ score, setScore ] = useState({ x: 0, y: 0 });
   const [ players, setPlayers ] = useState({ one: '', two:''})
 
   return (
-    <div className="App w-100 h-100">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" 
-        integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
-        crossOrigin="anonymous">
-      </link>
-      <Board p1={players.one} p2={players.two}/>
-      <Score p1={players.one} p2={players.two}/>
+    <div id={styles.box} className='d-flex justify-content-around flex-column align-items-center'>
+        <Board p1={players.one} p2={players.two}/>
+        <Score p1={players.one} p2={players.two}/>
     </div>
   );
 }
